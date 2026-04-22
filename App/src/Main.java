072d8d8c32008f09ca1c884803054f918a1c1165
Light-srm <rijulrose.05@gmail.com>
2026-04-22 09:20:24 +0530

    UC10

---
+++ App/src/Main.java
@@ -0,0 +1,13 @@
+class Bogie {
+    String type;
+    int capacity;
+
+    public Bogie(String type, int capacity) {
+        this.type = type;
+        this.capacity = capacity;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+}
